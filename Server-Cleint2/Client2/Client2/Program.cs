9618538d265a2b72ee70d93d5a2d83fc2993d1f6
xuyen97bg <39564099+xuyen97bg@users.noreply.github.com>
2020-10-03 01:08:46 +0700

    Add files via upload

---
+++ Server-Cleint2/Client2/Client2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress host = IPAddress.Parse("127.0.0.1");
+            IPEndPoint hostep = new IPEndPoint(host, 8000);
+            //Khai bao IP, Port
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Môi trương tạo kết nối - Kiểu kết nối - Kiểu giao thức
+            sock.Connect(hostep);
+            byte[] data = new byte[1024];
+            data = Encoding.UTF8.GetBytes("Ban tên là gì:");//Muốn gửi dữ liệu thì Chuyển dữ liệu sang byte. 
+            int n = SendVarData(sock, data);
+            sock.Close();
+            Console.WriteLine("Gui thanh cong %d" + n + "ki tu");
+        }
+        private static int SendVarData(Socket s, byte[] data)
+        {
+            int total = 0;
+            int size = data.Length;
+            int dataleft = size;
+            int sent;
+            byte[] datasize = new byte[4];
+            datasize = BitConverter.GetBytes(size); //Convert do dai du lieu tu int sang bit
+            sent = s.Send(datasize);//Gui do dai du kieu cho server
+            while (total < size)
+            {
+                sent = s.Send(data, total, dataleft, SocketFlags.None); // dữ liệu - tổng bit đã gửi - số bit gửi - Cờ
+                total += sent;
+                dataleft -= sent;
+            }
+            return total;
+        }
+
+    }
+}
---
+++ Server-Cleint2/Server/Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            IPHostEntry local = Dns.GetHostByName(Dns.GetHostName());
+            IPAddress host = IPAddress.Parse("127.0.0.1");
+            IPEndPoint hostep = new IPEndPoint(host, 8000);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            newsock.Bind(hostep); // Gan socket voi dia chi IP,Port cu the
+            newsock.Listen(2); // Toi da 2 ket noi client
+            Socket newclient = newsock.Accept();
+            byte[] data = new byte[17];
+            data = ReceiveVarData(newclient);
+            string quesion = Encoding.UTF8.GetString(data);
+            newsock.Close();
+            Console.WriteLine(quesion);
+           
+        }
+        private static byte[] ReceiveVarData(Socket s)
+        {
+            int total = 0;
+            int recv;
+            byte[] datasize = new byte[4];
+            recv = s.Receive(datasize, 0, 4, 0); // Nhận dữ liệu độ dài
+            int size = BitConverter.ToInt32(datasize);
+            int dataleft = size;
+            byte[] data = new byte[size];
+            while (total < size)
+            {
+                recv = s.Receive(data, total, dataleft, 0);
+                if (recv == 0)
+                {
+                    data = Encoding.ASCII.GetBytes("exit ");
+                    break;
+                }
+                total += recv;
+                dataleft -= recv;
+            }
+            return data;
+        }
+    }
+}
